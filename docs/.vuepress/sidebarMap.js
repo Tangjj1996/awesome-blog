700ef36dae76f6bc374f4dea2a0bb25388f71577
Tangjj1996 <Tangjj_1996@163.com>
2020-03-08 23:23:46 +0800

    fix: transform EE to BE

--- docs/.vuepress/sidebarMap.js
+++ docs/.vuepress/sidebarMap.js
@@ -1,6 +1,6 @@
 module.exports = [
     { title: "前端源码分析", link: 'FE' },
-    { title: "后端小书", link: "EE" },
+    { title: "后端小书", link: "BE" },
     { title: "博客", link: 'blog' },
     { title: '优秀博文', link: 'lib' }
 ]
